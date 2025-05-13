5acca7563a065dcc37d046de9819e56f4e7a7a1d
EasyNickHub <maximkrabik96@gmail.com>
2025-05-13 18:46:31 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    double M, t, af, qp, qf, qt;
+
+    cout << "Введите суточную выработку цеха (M): "; cin >> M;
+    cout << "Введите время смены (t): "; cin >> t;
+    cout << "Введите массовую долю фарша (af): "; cin >> af;
+    cout << "Введите производительность пельменей (qp): "; cin >> qp;
+    cout << "Введите производительность фарша (qf): "; cin >> qf;
+    cout << "Введите производительность теста (qt): "; cin >> qt;
+
+    if (M <= 0  af < 0  qp <= 0  qt <= 0) {
+        cerr << "Ошибка: недопустимые входные данные!" << endl;
+        return 1;
+    }
+
+    double Qp = M / 2 / t;
+    double np = Qp / qp;
+
+    double Qf = af * Qp;
+    double nf = Qf / qf;
+
+    double Qt = (1 - af) * Qp;
+    double nt = Qt / qt;
+
